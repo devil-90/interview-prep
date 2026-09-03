ab456e8640409037f6929cf4d73b856a63ee2554
Laya <140315910+devil-90@users.noreply.github.com>
2026-09-03 13:12:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (88.93%) - LeetSync

--- owlsure/arrays/hashmap-hashset/2654-count-the-number-of-vowel-strings-in-range/count-the-number-of-vowel-strings-in-range.py
+++ owlsure/arrays/hashmap-hashset/2654-count-the-number-of-vowel-strings-in-range/count-the-number-of-vowel-strings-in-range.py
@@ -1,6 +1,6 @@
 class Solution:
     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
-        vowels = "aeiou"
+        vowels = {"a","e","i","o","u"}
         count = 0
         for i in range(left, right+1):
             word = words[i]
