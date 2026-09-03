b04e782e649e8a47aa50f537767b31f1732d254e
Laya <140315910+devil-90@users.noreply.github.com>
2026-09-03 11:07:34 +0530

    Time: 4 ms (59.32%) | Memory: 20.5 MB (62.91%) - LeetSync

--- owlsure/arrays/hashmap-hashset/283-move-zeroes/move-zeroes.py
+++ owlsure/arrays/hashmap-hashset/283-move-zeroes/move-zeroes.py
@@ -7,8 +7,10 @@ class Solution:
 
         for i in range(len(nums)):
             if nums[i] != 0:
-                temp = nums[i]
-                nums[i] = nums[z]
-                nums[z] = temp
+                # temp = nums[i]
+                # nums[i] = nums[z]
+                # nums[z] = temp
+                # z+=1
+                nums[i], nums[z] = nums[z], nums[i]
                 z+=1
 
