1dcef2de5d3cfb20551f883c19aafb67445c1d66
Laya <140315910+devil-90@users.noreply.github.com>
2026-09-03 11:55:37 +0530

    Time: 3 ms (44.06%) | Memory: 20.3 MB (99.65%) - LeetSync

--- owlsure/arrays/hashmap-hashset/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ owlsure/arrays/hashmap-hashset/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        j = 0
-        for num in range(1, len(nums)):
-            if nums[num] != nums[j]:
+        j=0
+        for i in range(1,len(nums)):
+            if nums[i] != nums[j]:
                 j+=1
-                nums[j] = nums[num]
+                nums[j] = nums[i]
                 
 
         return j+1
