eaddb68250670b1e74f76608249969781262b186
Laya <140315910+devil-90@users.noreply.github.com>
2026-09-19 23:28:51 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (42.13%) - LeetSync

--- owlsure/arrays/hashmap-hashset/1-two-sum/two-sum.py
+++ owlsure/arrays/hashmap-hashset/1-two-sum/two-sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
-        for num in range(len(nums)):
-            difference = target - nums[num]
-            if difference in seen:
-                return [seen[difference], num]
-            seen[nums[num]] = num
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in seen:
+                return [seen[diff], i]
+            seen[nums[i]] = i
+        return -1
